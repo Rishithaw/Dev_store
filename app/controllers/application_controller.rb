5b500654e4db8855a2610b9d2e94d6f627472003
rishithaw <rishithaw1@gmail.com>
2025-11-30 10:43:32 -0600

    Fixed Sign Up Issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :current_user, :user_signed_in?
 
   protected
 
   def configure_permitted_parameters
     added_attrs = [
-      :username, :address, :city, :postal_code,
-      :email, :password, :password_confirmation, :remember_me
+      :username, :shipping_street, :shipping_city, :shipping_province_id, :shipping_postal_code, :email, :password, :password_confirmation
     ]
 
     devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
---
+++ db/migrate/20251130164217_add_missing_devise_fields_to_users.rb
@@ -0,0 +1,4 @@
+class AddMissingDeviseFieldsToUsers < ActiveRecord::Migration[8.0]
+  def change
+  end
+end
